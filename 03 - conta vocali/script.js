fd63b0cb4f12099105f71ff8b253c9739296d6fd
Orsouene <orsoueneelaouizeb@gmail.com>
2024-11-06 14:21:10 +0100

    terzo esercizio CONTA VOCALI

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,23 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
+const word = "javascript";
 
+let parola = prompt("inserisce una parola");
+const vocali = ["a", "e", "i", "o", "u"];
 
 // Dichiara la funzione qui.
-
-
-// Invoca la funzione qui e stampa il risultato in console
-
-
-
+function VerificazioneVocali(parola, vocali) {
+  // Invoca la funzione qui e stampa il risultato in console
+  let liste = [];
+  for (let i = 0; i < parola.length; i++) {
+    for (let j = 0; j < vocali.length; j++) {
+      if (parola[i] === vocali[j]) {
+        liste.push(vocali[j]);
+      }
+    }
+  }
+  return liste;
+}
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+const result = VerificazioneVocali(parola, vocali);
+console.log(result);
