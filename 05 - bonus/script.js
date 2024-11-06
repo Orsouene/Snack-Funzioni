5207d9d0532f76f7928d8a40f774376bd1427691
Orsouene <orsoueneelaouizeb@gmail.com>
2024-11-06 14:54:26 +0100

    Bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -5,14 +5,23 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
-
+const name = "Mario";
+let nome = prompt("Scrive il tuo nome ");
+const d = new Date();
 // Dichiara la funzione qui.
-
-
-// Invoca la funzione qui e stampa il risultato in console
-
-
-
+function salutare(nome) {
+  // Invoca la funzione qui e stampa il risultato in console
+  let saluto = "";
+  let hour = d.getHours();
+  if (hour > 4 && hour < 13) {
+    saluto = "buongiorno " + nome;
+  } else if (hour > 13 && hour < 17) {
+    saluto = "buon pomeriggio " + nome;
+  } else if (hour > 17 && hour < 4) {
+    saluto = "buonasera " + nome;
+  }
+  return saluto;
+}
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+const resultato = salutare(nome);
+console.log(resultato);
