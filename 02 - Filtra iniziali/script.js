24e25b1699d91119f95d1f176cac3893f5eaa88b
Orsouene <orsoueneelaouizeb@gmail.com>
2024-11-06 13:47:07 +0100

    Secondo esercizio FILTRA INIZIALI

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -2,12 +2,19 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
+let lettera = prompt("inserisce una lettera ").toUpperCase();
 // Dichiara la funzione qui.
+function myArray(names, lettera) {
+  // Invoca la funzione qui e stampa il risultato in console
+  newArray = [];
 
-
-// Invoca la funzione qui e stampa il risultato in console
-
-
-
+  for (let i = 0; i < names.length; i++) {
+    if (names[i].charAt(0) === lettera) {
+      newArray.push(names[i]);
+    }
+  }
+  return newArray;
+}
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+const finalArray = myArray(names, lettera);
+console.log(finalArray);
