58d1a2b41329b8c7c5b19355455c1a0877aca12a
Orsouene <orsoueneelaouizeb@gmail.com>
2024-11-06 14:36:16 +0100

    Quarto esercizio SALUTI

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,14 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
+const name = "Mario";
 
-
+let nome = prompt("Scrive il tuo nome ");
+let saluto = "";
 // Dichiara la funzione qui.
-
-
-// Invoca la funzione qui e stampa il risultato in console
-
-
+const salutare = (nome, saluto) =>
+  // Invoca la funzione qui e stampa il risultato in console
+  (saluto = "Ciao " + nome);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
+const resultato = salutare(nome);
+console.log(resultato);
